d4d219767fcd1d6b43ce657e83041973c389e6f0
same-woo <pardongwoo@naver.com>
2023-06-07 01:15:18 +0900

    0607

--- java-lang/app/src/main/java/bitcamp/project/app/src/main/java/bitcamp/myapp/Test.java
+++
@@ -1,9 +0,0 @@
-package bitcamp.myapp;
-
-public class Test {
-  public static void main(String[] args) {
-    int a = 100, b = 200;
-    swap(a, b);
-    System.out.printf("%d, %d\n", a, b);
-  }
-}
