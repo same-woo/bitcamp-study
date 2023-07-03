b64cd0ce516787c2279bed4f381b790b01b59fff
same-woo <pardongwoo@naver.com>
2023-07-03 09:45:25 +0900

    "0703"

--- myapp/app/src/main/java/bitcamp/myapp/myproject/handler/Center/TrainingCenterUpdateListener.java
+++ myapp/app/src/main/java/bitcamp/myapp/myproject/handler/Center/TrainingCenterUpdateListener.java
@@ -4,13 +4,11 @@ import java.util.List;
 import bitcamp.myapp.myproject.vo.TrainingCenter;
 import bitcamp.util.BreadcrumbPrompt;
 
-
 public class TrainingCenterUpdateListener extends AbstractTrainingCenterListener {
 
   private String temp = "";
 
   public TrainingCenterUpdateListener(List<TrainingCenter> list) {
-
     super(list);
   }
 
@@ -23,7 +21,6 @@ public class TrainingCenterUpdateListener extends AbstractTrainingCenterListener
       System.out.println("해당 번호의 수강생이 없습니다!");
       return;
     }
-
     System.out.println("어떤 정보를 바꾸시겠습니까 ?");
     System.out.println("(0) 전체변경");
     System.out.println("(1) 이름 변경");
@@ -40,7 +37,7 @@ public class TrainingCenterUpdateListener extends AbstractTrainingCenterListener
         System.out.printf("기존 : (%s) -> (%s). 변경완료\n", temp, center.getName());
         break;
       case 2:
-        temp = Integer.toString((center.getAge()));
+        temp = Integer.toString(center.getAge());
         center.setAge(prompt.inputInt("나이(%d)? ", center.getAge()));
         System.out.printf("기존 : (%s) -> (%d). 변경완료\n", temp, center.getAge());
         break;
@@ -50,8 +47,8 @@ public class TrainingCenterUpdateListener extends AbstractTrainingCenterListener
         System.out.printf("기존 : (%s) -> (%s). 변경완료\n", temp, center.getLocation());
         break;
       case 4:
-        temp = Integer.toString((center.getDuration()));
-        center.setDuration(prompt.inputInt("기간(%s)? ", center.getDuration()));
+        temp = Integer.toString(center.getDuration());
+        center.setDuration(prompt.inputInt("기간(%d)? ", center.getDuration()));
         System.out.printf("기존 : (%s) -> (%d). 변경완료\n", temp, center.getDuration());
         break;
       case 5:
@@ -62,14 +59,13 @@ public class TrainingCenterUpdateListener extends AbstractTrainingCenterListener
       case 0:
         System.out.println("전체 내용을 변경합니다.");
         center.setName(prompt.inputString("이름(%s)? ", center.getName()));
-        center.setAge(prompt.inputInt("나이(%s)? ", center.getAge()));
+        center.setAge(prompt.inputInt("나이(%d)? ", center.getAge()));
         center.setLocation(prompt.inputString("지역(%s)? ", center.getLocation()));
-        center.setDuration(prompt.inputInt("기간(%s" + "개월)?", center.getDuration()));
+        center.setDuration(prompt.inputInt("기간(%d)? ", center.getDuration()));
         center.setCurriculum(prompt.inputString("커리큘럼(%s)? ", center.getCurriculum()));
         break;
       default:
         break;
     }
   }
-
 }
