784b9b45276b7cdd3ab1fe4c63eb0b6822aea2df
same-woo <pardongwoo@naver.com>
2023-07-07 18:14:45 +0900

    "0707"

--- ExamProject/app-client/src/main/java/bitcamp/dao/DaoBuilder.java
+++ ExamProject/app-client/src/main/java/bitcamp/dao/DaoBuilder.java
@@ -2,7 +2,6 @@ package bitcamp.dao;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
-import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Proxy;
@@ -24,47 +23,42 @@ public class DaoBuilder {
   @SuppressWarnings("unchecked")
   public <T> T build(String dataName, Class<T> type) {
 
+
     return (T) Proxy.newProxyInstance(ClientApp.class.getClassLoader(), new Class[] {type},
-        new InvocationHandler() {
+        (Object proxy, Method method, Object[] args) -> {
+          // 요청 정보 준비
+          RequestEntity requestEntity = new RequestEntity();
+          requestEntity.command(dataName + "/" + method.getName());
+          if (args != null) {
+            requestEntity.data(args[0]);
+          }
 
+          System.out.println(requestEntity.toJson());
 
+          // 요청 정보 전송
+          out.writeUTF(requestEntity.toJson());
 
-          @Override
-          public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-            // 요청 정보 준비
-            RequestEntity requestEntity = new RequestEntity();
-            requestEntity.command(dataName + "/" + method.getName());
-            if (args != null) {
-              requestEntity.data(args[0]);
-            }
+          // 응답 정보 수신
+          ResponseEntity response = ResponseEntity.fromJson(in.readUTF());
+          if (response.getStatus().equals(ResponseEntity.ERROR)) {
+            throw new RuntimeException(response.getResult());
+          }
 
-            System.out.println(requestEntity.toJson());
+          // 리턴 타입 조사
+          Class<?> returnType = method.getReturnType();
 
-            // 요청 정보 전송
-            out.writeUTF(requestEntity.toJson());
+          if (returnType == int.class) {
+            return response.getObject(int.class);
 
-            // 응답 정보 수신
-            ResponseEntity response = ResponseEntity.fromJson(in.readUTF());
-            if (response.getStatus().equals(ResponseEntity.ERROR)) {
-              throw new RuntimeException(response.getResult());
-            }
+          } else if (returnType == void.class) {
+            return null;
 
-            // 리턴 타입 조사
-            Class<?> returnType = method.getReturnType();
-
-            if (returnType == int.class) {
-              return response.getObject(int.class);
-
-            } else if (returnType == void.class) {
-              return null;
-
-            } else if (returnType == List.class) {
-              ParameterizedType paramType = (ParameterizedType) method.getGenericReturnType();
-              Class<?> itemType = (Class<?>) paramType.getActualTypeArguments()[0];
-              return response.getList(itemType);
-            } else {
-              return response.getObject(returnType);
-            }
+          } else if (returnType == List.class) {
+            ParameterizedType paramType = (ParameterizedType) method.getGenericReturnType();
+            Class<?> itemType = (Class<?>) paramType.getActualTypeArguments()[0];
+            return response.getList(itemType);
+          } else {
+            return response.getObject(returnType);
           }
         });
   }
