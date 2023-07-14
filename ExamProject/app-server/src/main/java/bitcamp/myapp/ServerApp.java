3226aa6d51267e93ba896fbb00c5f69643d2452b
same-woo <pardongwoo@naver.com>
2023-07-14 17:10:17 +0900

    "0714 ThreadPool"

--- ExamProject/app-server/src/main/java/bitcamp/myapp/ServerApp.java
+++ ExamProject/app-server/src/main/java/bitcamp/myapp/ServerApp.java
@@ -12,6 +12,9 @@ import bitcamp.myapp.dao.BoardListDao;
 import bitcamp.myapp.dao.MemberListDao;
 import bitcamp.net.RequestEntity;
 import bitcamp.net.ResponseEntity;
+import bitcamp.util.Job;
+import bitcamp.util.ManagedThread;
+import bitcamp.util.ThreadPool;
 
 // 1) 클라이언트가 보낸 명령을 데이터 이름과 메서드 이름으로 분리한다.
 // 2) 클라이언트가 요청한 DAO 객체와 메서드를 찾는다.
@@ -23,13 +26,14 @@ public class ServerApp {
   int port;
   ServerSocket serverSocket;
 
-  // 클라이언트 요청을 처리 할 DAO 객체를 맵에 보관한다.
   HashMap<String, Object> daoMap = new HashMap<>();
+
+  // 스레드를 리턴해 줄 스레드풀 준비
+  ThreadPool threadPool = new ThreadPool();
 
   public ServerApp(int port) throws Exception {
     this.port = port;
 
-    // DAO 객체 생성 및 보관
     daoMap.put("member", new MemberListDao("member.json"));
     daoMap.put("board", new BoardListDao("board.json"));
     daoMap.put("reading", new BoardListDao("reading.json"));
@@ -51,19 +55,18 @@ public class ServerApp {
   }
 
   public void execute() throws Exception {
-    class RequestAgentThread extends Thread {
+    class RequestProcessJob implements Job {
       Socket socket;
 
-      public RequestAgentThread(Socket socket) {
+      public RequestProcessJob(Socket socket) {
         this.socket = socket;
       }
 
       @Override
-      public void run() {
+      public void execute() {
         processRequest(socket);
       }
     }
-
 
     System.out.println("[MyList 서버 애플리케이션]");
 
@@ -71,12 +74,13 @@ public class ServerApp {
     System.out.println("서버 실행 중...");
 
     while (true) {
-      new RequestAgentThread(serverSocket.accept()).start();
+      Socket socket = serverSocket.accept();
+      ManagedThread t = threadPool.getResource();
+      t.setJob(new RequestProcessJob(socket));
     }
   }
 
   public static Method findMethod(Object obj, String methodName) {
-    // DAO에 해당 메서드가 있는지 알아낸다.
     Method[] methods = obj.getClass().getDeclaredMethods();
     for (int i = 0; i < methods.length; i++) {
       if (methods[i].getName().equals(methodName)) {
@@ -86,69 +90,58 @@ public class ServerApp {
     return null;
   }
 
-  // 메서드 호출하기
   public static Object call(Object obj, Method method, RequestEntity request) throws Exception {
     Parameter[] params = method.getParameters();
     if (params.length > 0) {
-      @SuppressWarnings("unused")
-      Object arg = request.getObject(params[0].getType());
       return method.invoke(obj, request.getObject(params[0].getType()));
     } else {
       return method.invoke(obj);
     }
   }
 
-  // 클라이언트와 접속이 이루어지면 클라이언트의 요청을 처리한다.
   public void processRequest(Socket socket) {
-
-    try (Socket s = socket; // 사용할 용도 외에 오류 시에 자원을 닫아주는 용도로 명시
+    try (Socket s = socket;
         DataInputStream in = new DataInputStream(socket.getInputStream());
-        DataOutputStream out = new DataOutputStream(socket.getOutputStream());) {
+        DataOutputStream out = new DataOutputStream(socket.getOutputStream())) {
 
       InetSocketAddress socketAddress = (InetSocketAddress) socket.getRemoteSocketAddress();
-      System.out.printf("%s:%s 클라이언트가 접속했음!\n", socketAddress.getHostName(),
+      System.out.printf("%s:%s 클라이언트가 접속했음!\n", socketAddress.getHostString(),
           socketAddress.getPort());
-
+      Thread.sleep(100);
       // 클라이언트 요청을 반복해서 처리하지 않는다.
-      // 요청 -> 실행 -> 응답 ->
+      // => 접속 -> 요청 -> 실행 -> 응답 -> 연결 끊기
       RequestEntity request = RequestEntity.fromJson(in.readUTF());
 
       String command = request.getCommand();
       System.out.println(command);
 
-      // 데이터 이름과 메서드 이름 알아내기
       String[] values = command.split("/");
       String dataName = values[0];
       String methodName = values[1];
 
-      // 데이터 이름으로 DAO 객체를 꺼낸다.
       Object dao = daoMap.get(dataName);
-      if (dao == null) { // 만약 데이터를 처리 할 DAO를 찾지 못한다면 오류 정보를 클라이언트에게 보낸다.
+      if (dao == null) {
         out.writeUTF(
             new ResponseEntity().status(ResponseEntity.ERROR).result("데이터를 찾을 수 없습니다.").toJson());
         return;
       }
-      // Dao객체에서 메서드 찾기
+
       Method method = findMethod(dao, methodName);
       if (method == null) {
-        // 만약 클라이언트가 요청한 메서드를 찾지 못한다면 오류 정보를 클라이언트에게 보낸다.
         out.writeUTF(
             new ResponseEntity().status(ResponseEntity.ERROR).result("메서드를 찾을 수 없습니다.").toJson());
         return;
       }
 
       try {
-        // DAO 메서드 호출하기
         Object result = call(dao, method, request);
 
-        // 메서드 호출 결과를 클라이언트에게 보낸다.
         ResponseEntity response = new ResponseEntity();
         response.status(ResponseEntity.SUCCESS);
         response.result(result);
         out.writeUTF(response.toJson());
 
       } catch (Exception e) {
-        // 메서드 호출 결과를 클라이언트에게 보낸다.
         ResponseEntity response = new ResponseEntity();
         response.status(ResponseEntity.ERROR);
         response.result(e.getMessage());
@@ -158,7 +151,4 @@ public class ServerApp {
       System.out.println(e.getMessage());
     }
   }
-
 }
-
-
---
+++ ExamProject/app-server/src/main/java/bitcamp/util/ResourcePool.java
@@ -0,0 +1,7 @@
+package bitcamp.util;
+
+public interface ResourcePool<T> {
+  T getResource();
+
+  void returnResource(T resource);
+}
