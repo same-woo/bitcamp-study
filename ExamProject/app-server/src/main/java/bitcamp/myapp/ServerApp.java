a714fa990358157038f07b830b63de16e3027c52
same-woo <pardongwoo@naver.com>
2023-07-17 15:24:40 +0900

    "0717"

--- ExamProject/app-server/src/main/java/bitcamp/myapp/ServerApp.java
+++ ExamProject/app-server/src/main/java/bitcamp/myapp/ServerApp.java
@@ -8,12 +8,12 @@ import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.HashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import bitcamp.myapp.dao.BoardListDao;
 import bitcamp.myapp.dao.MemberListDao;
 import bitcamp.net.RequestEntity;
 import bitcamp.net.ResponseEntity;
-import bitcamp.util.ManagedThread;
-import bitcamp.util.ThreadPool;
 
 // 1) 클라이언트가 보낸 명령을 데이터 이름과 메서드 이름으로 분리한다.
 // 2) 클라이언트가 요청한 DAO 객체와 메서드를 찾는다.
@@ -27,8 +27,8 @@ public class ServerApp {
 
   HashMap<String, Object> daoMap = new HashMap<>();
 
-  // 스레드를 리턴해 줄 스레드풀 준비
-  ThreadPool threadPool = new ThreadPool();
+  // 자바 스레드풀 준비
+  ExecutorService threadPool = Executors.newFixedThreadPool(10);
 
   public ServerApp(int port) throws Exception {
     this.port = port;
@@ -62,8 +62,14 @@ public class ServerApp {
 
     while (true) {
       Socket socket = serverSocket.accept();
-      ManagedThread t = threadPool.getResource();
-      t.setJob(() -> processRequest(socket));
+
+      threadPool.execute(new Runnable() {
+        @Override
+        public void run() {
+          processRequest(socket);
+        }
+      });
+
     }
   }
 
@@ -92,8 +98,8 @@ public class ServerApp {
         DataOutputStream out = new DataOutputStream(socket.getOutputStream())) {
 
       InetSocketAddress socketAddress = (InetSocketAddress) socket.getRemoteSocketAddress();
-      System.out.printf("%s:%s 클라이언트가 접속했음!\n", socketAddress.getHostString(),
-          socketAddress.getPort());
+      System.out.printf("[%s] $s:%s 클라이언트가 접속했음!\n", Thread.currentThread().getName(),
+          socketAddress.getHostString(), socketAddress.getPort());
       Thread.sleep(100);
       // 클라이언트 요청을 반복해서 처리하지 않는다.
       // => 접속 -> 요청 -> 실행 -> 응답 -> 연결 끊기
