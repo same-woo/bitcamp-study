c749823109a9fb1e19cdc0e33aa69d886aa89109
same-woo <pardongwoo@naver.com>
2023-08-22 12:22:29 +0900

    "0822"

---
+++ eomcs-servlet/app/src/main/java/eomcs/web/ex06/Servlet05.java
@@ -0,0 +1,19 @@
+package eomcs.web.ex06;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class Servlet05 extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
---
+++ eomcs-servlet/app/src/main/java/exomcs/servlet/ex06/Filter01.java
@@ -0,0 +1,17 @@
+package exomcs.servlet.ex06;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+public class Filter01 implements Filter {
+    public void init(FilterConfig config) throws ServletException {
+    }
+
+    public void destroy() {
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws ServletException, IOException {
+        chain.doFilter(request, response);
+    }
+}
